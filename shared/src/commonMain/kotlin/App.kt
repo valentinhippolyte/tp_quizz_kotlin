e970e2e54c86cc038501e426f0d5516f615645ea
Valentin Hippolyte <valenthippo@gmail.com>
2023-11-08 19:38:10 +0100

    test commit

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -9,7 +9,7 @@ fun App() {
     MaterialTheme {
        /* val welcome = Welcome("APPQUIZZ", "Bienvenue dans la meilleur app !")*/
         val proposals: Array<String> = arrayOf("1", "2", "3", "4")
-        val questions: Array<String> = arrayOf("Quest1", "Quest2", "Quest3", "Quest4", "Quest5", "Quest6")
+        val questions: Array<String> = arrayOf("Question 1", "Quest2", "Quest3", "Quest4", "Quest5", "Quest6")
 
         QuestionComposable(questions, proposals, 2, 1)
         /*ScoreComposable(
