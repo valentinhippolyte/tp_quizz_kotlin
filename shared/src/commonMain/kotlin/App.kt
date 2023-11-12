7d81cee7b2eb039cf12effef5df5a2ceaf472fd9
Valentin Hippolyte <valenthippo@gmail.com>
2023-11-12 19:21:55 +0100

    add some changes (styles+questions)

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -15,35 +15,35 @@ fun App() {
            listOf(
                Question(
                    0,
-                   "Question 1",
-                   0,
+                   "Qu'est-ce qu'un langage de programmation ?",
+                   3,
                    listOf(
-                       Answer(0,"A"),
-                       Answer(1,"B"),
-                       Answer(2,"C"),
-                       Answer(3,"D"),
+                       Answer(0,"Un manuel d'instructions"),
+                       Answer(1,"Un outil pour réparer les ordinateurs"),
+                       Answer(2,"Une méthode de communication entre les humains"),
+                       Answer(3,"Un ensemble de règles grammaticales pour écrire du code"),
                    )
                ),
                Question(
                    1,
-                   "Question 2",
-                   0,
+                   "Quel est le rôle d'un fichier avec l'extension \".html\" dans le développement web ?",
+                   1,
                    listOf(
-                       Answer(0,"A"),
-                       Answer(1,"B"),
-                       Answer(2,"C"),
-                       Answer(3,"D"),
+                       Answer(0,"Stocker des données sensibles"),
+                       Answer(1,"Définir la structure d'une page web"),
+                       Answer(2,"Exécuter des scripts côté serveur"),
+                       Answer(3,"Contrôler les animations sur une page"),
                    )
                ),
                Question(
                    2,
-                   "Question 3",
-                   0,
+                   "Qu'est-ce qu'une boucle dans la programmation?",
+                   1,
                    listOf(
-                       Answer(0,"A"),
-                       Answer(1,"B"),
-                       Answer(2,"C"),
-                       Answer(3,"D"),
+                       Answer(0,"Un bug difficile à résoudre"),
+                       Answer(1,"Une structure de contrôle répétitive"),
+                       Answer(2,"Un type de virus informatique"),
+                       Answer(3,"Un moyen de crypter des données"),
                    )
                )
            )
--- shared/src/commonMain/kotlin/screens/QuestionScreen.kt
+++ shared/src/commonMain/kotlin/screens/QuestionScreen.kt
@@ -1,23 +1,33 @@
 package screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dataClass.Quiz
 
@@ -33,66 +43,80 @@ fun QuestionScreen(
     var selectedAnswer by remember { mutableStateOf(1) }
     var score by remember { mutableStateOf(0) }
 
-
-    Card(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Column(
+        Card(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(16.dp)
         ) {
             Text(
                 text = quiz.questions[questionIndex].label,
                 style = MaterialTheme.typography.h4,
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier
+                    .padding(8.dp),
+                textAlign = TextAlign.Center
             )
+        }
 
-            quiz.questions[questionIndex].answers.forEachIndexed { index, answer ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                        .clickable {selectedAnswer = answer.id}
-                ) {
-                    RadioButton(
-                        selected = selectedAnswer == answer.id,
-                        onClick = { selectedAnswer = answer.id}
-                    )
-
-                    Text(text = answer.label,
-                        style = MaterialTheme.typography.body1,
-                        modifier = Modifier.width(8.dp),
-                    )
-                }
-            }
-
-            Button(
-                onClick = {
-                    if (selectedAnswer == quiz.questions[questionIndex].correctId) {
-                        score++
-                    }
-                    questionIndex += 1
-                },
+        quiz.questions[questionIndex].answers.forEachIndexed { index, answer ->
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
+                    .clickable { selectedAnswer = answer.id }
+
             ) {
-                Text(text = "Valider")
-            }
-            LinearProgressIndicator(
-                progress = (questionIndex.toFloat() / quiz.questions.size.toFloat()),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            )
-            Row {
+                RadioButton(
+                    selected = selectedAnswer == answer.id,
+                    onClick = { selectedAnswer = answer.id }
+                )
+
                 Text(
-                    text = "score : " + score.toString()
+                    text = answer.label,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp),
                 )
             }
         }
-    }
 
+        Button(
+            onClick = {
+                if (selectedAnswer == quiz.questions[questionIndex].correctId) {
+                    score++
+                }
+                questionIndex += 1
+                selectedAnswer = 0
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null, tint = Color.White)
+                Text(text = "Next", modifier = Modifier.padding(start = 8.dp))
+            }
+        }
+        LinearProgressIndicator(
+            progress = (questionIndex.toFloat() / quiz.questions.size.toFloat()),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+        Row {
+            Text(
+                text = "score : " + score.toString() + "/" + questionIndex.toInt()
+            )
+        }
+    }
 }
